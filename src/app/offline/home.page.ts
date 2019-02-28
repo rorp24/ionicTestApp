1cfd90ead15da1b26ff1722fa4096e19f098d84d
robin ELZEARD <robinelzeard@gmail.com>
2019-02-28 14:41:24 +0100

    page offline (a connecter)

---
+++ src/app/offline/home.page.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+ 
+  users = [];
+ 
+  constructor(private apiService: ApiService, private plt: Platform) { }
+ 
+  ngOnInit() {
+    this.plt.ready().then(() => {
+      this.loadData(true);
+    });
+  }
+ 
+  loadData(refresh = false, refresher?) {
+    this.apiService.getUsers(refresh).subscribe(res => {
+      this.users = res;
+      if (refresher) {
+        refresher.target.complete();
+      }
+    });
+  }
+ 
+  updateUser(id) {
+    this.apiService.updateUser(id, {name: 'Simon', job: 'CEO'}).subscribe();
+  }
+}
