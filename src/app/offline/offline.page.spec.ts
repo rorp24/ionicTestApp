f5f3d46419c43cd8b199b3881fcb8366bb948890
robin ELZEARD <robinelzeard@gmail.com>
2019-02-28 15:20:37 +0100

    correction mineure (ça provoquais pas de bug, mais dans le doute...)

--- src/app/offline/offline.page.spec.ts
+++ src/app/offline/offline.page.spec.ts
@@ -1,15 +1,15 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomePage } from './home.page';
+import { HomePage } from './offline.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
+describe('OfflinePage', () => {
+  let component: OfflinePage;
   let fixture: ComponentFixture<HomePage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ OfflinePage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
       .compileComponents();
